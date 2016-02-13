d5deb80cbd574ce18dde119b97df6d830e723d2f
Vadim Stupakov <vadim.stupakov@gmail.com>
2016-02-13 14:47:57 +0200

    Added OPENOCD_CFG_FILE to users.config.sh

--- users.config.sh
+++ users.config.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Insert your variables here
+OPENOCD_CFG_FILE=""
 PROJECT_PATH=""
 STM32_CUBE_MX_INCLUDE_PATH=""
 DEFINES=""
 
 # Examples:
+# OPENOCD_CFG_FILE="/usr/share/openocd/scripts/board/stm32f4discovery.cfg"
 # PROJECT_PATH="/path/to/project/that/contains/filename.ioc"
 # STM32_CUBE_MX_INCLUDE_PATH="/full/path/to/STM32Cube/Repository/STM32Cube_FW_F4_V1.10.0"
 # DEFINES="STM32F407xx USE_HAL_DRIVER"

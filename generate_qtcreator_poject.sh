6e6557aa32a6b0dc512a6a1c5c6017b9a202bcf2
Vadim Stupakov <vadim.stupakov@gmail.com>
2016-02-28 23:19:50 +0200

    Adding getting parameters like sources and includes from Makefile

--- generate_qtcreator_poject.sh
+++ generate_qtcreator_poject.sh
@@ -2,22 +2,18 @@
 
 source ./users.config.sh
 
-if [ "$DEFINES" == "" ] ||
-   [ "$PROJECT_PATH" == "" ] ||
+if [ "$PROJECT_PATH" == "" ] ||
    [ "$OPENOCD_CFG_FILE" == "" ]; then
     echo "Please, set variables in the users.config.sh!"
     exit -1
 fi
 
-if [ ! -d "$STM32_CUBE_MX_INCLUDE_PATH" ]; then
-    echo "The STM32_CUBE_MX_INCLUDE_PATH var path doesn't exist."
-    exit -1
-elif [ ! -d "$PROJECT_PATH" ]; then
+if [ ! -d "$PROJECT_PATH" ]; then
     echo "The PROJECT_PATH var doesn't exit"
     exit -1
 fi
 
-sudo chown -R $USER:$USER $PROJECT_PATH/*
+sudo sh -c "find -L $PROJECT_PATH | xargs -I{} chown $USER:$USER {}"
 find -L $PROJECT_PATH -type f | xargs -I{} chmod 666 {}
 
 PROJECT_NAME="$(find -L "$PROJECT_PATH" -type f -regex '.*\.ioc' | sed -e 's/\(.*\)\.ioc/\1/g')"
@@ -28,16 +24,24 @@ else
     PROJECT_NAME=$(basename "$PROJECT_NAME")
 fi
 
-FILES=$(find -L "$PROJECT_PATH" -type f | grep -E ".*(\.(c|cpp|s|S|asm|h)|Makefile)$")
-INCLUDES=$(find -L $PROJECT_PATH -type f -regex ".*\.h$" | xargs dirname | sort | uniq)
+# Generating Makefile for SW4STM32 project
+./CubeMX2Makefile/CubeMX2Makefile.py "$PROJECT_PATH" "$STM32_CUBE_MX_INCLUDE_PATH"
+
+FILES=$(make -f "$PROJECT_PATH/Makefile" C_SOURCES_PRINT ASM_SOURCES_PRINT)
+INCLUDES=$(make -f "$PROJECT_PATH/Makefile" C_INCLUDES_PRINT ASM_INCLUDES_PRINT)
+DEFINES=$(make -f "$PROJECT_PATH/Makefile" C_DEFS_PRINT ASM_DEFS_PRINT)
 
 printf "%s\n" $FILES           > "$PROJECT_PATH/$PROJECT_NAME.files"
 printf "%s\n" $INCLUDES        > "$PROJECT_PATH/$PROJECT_NAME.includes"
 printf "[General]\n"           > "$PROJECT_PATH/$PROJECT_NAME.creator"
 printf "#define %s\n" $DEFINES > "$PROJECT_PATH/$PROJECT_NAME.config"
 
-# Generating Makefile for SW4STM32 project
-./CubeMX2Makefile/CubeMX2Makefile.py "$PROJECT_PATH" "$STM32_CUBE_MX_INCLUDE_PATH"
+# Removing -I from include file
+sed -i "s/^-I//g" "$PROJECT_PATH/$PROJECT_NAME.includes"
+
+# Removing -D from define file
+sed -i "s/-D//g" "$PROJECT_PATH/$PROJECT_NAME.config"
+
 
 printf "\
 #!/bin/bash\n\

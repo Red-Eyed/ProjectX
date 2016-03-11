c0ffa4842732153a3f34a32f6afb8c5b472a7af0
Vadim Stupakov <vadim.stupakov@gmail.com>
2016-03-11 19:01:00 +0200

    Added STM32_CUBEMX_PATH

--- users.config.sh
+++ users.config.sh
@@ -2,11 +2,11 @@
 
 # Insert your variables here
 OPENOCD_CFG_FILE="/usr/share/openocd/scripts/board/stm32f4discovery.cfg"
-PROJECT_PATH="/home/vadimstupakov/GIT/ProjectX/Examples/STM32F4Discovery_USB_Custom_HID_template/"
-DEFINES="STM32F407xx USE_HAL_DRIVER"
+PROJECT_PATH="/home/vadimstupakov/GIT/ProjectX/Examples/Boards/STM32F4Discovery/Timer_LED_Blinking/"
+STM32_CUBE_MX_INCLUDE_PATH="$HOME/Data/STM32/STM32Cube/Repository/STM32Cube_FW_F4_V1.10.0/"
+
 
 # Examples:
 # OPENOCD_CFG_FILE="/usr/share/openocd/scripts/board/stm32f4discovery.cfg"
 # PROJECT_PATH="/path/to/project/that/contains/filename.ioc"
-# DEFINES="STM32F407xx USE_HAL_DRIVER"
-
+# STM32_CUBE_MX_INCLUDE_PATH="$HOME/Data/STM32/STM32Cube/Repository/STM32Cube_FW_F4_V1.10.0/"

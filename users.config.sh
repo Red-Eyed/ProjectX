6c394d3fb6d9201da54a6be012a78d6b1d8feaa4
Vadim-Stupakov <vadim.stupakov@gmail.com>
2016-04-03 11:55:04 +0300

    Changed absolute path to real path

--- users.config.sh
+++ users.config.sh
@@ -2,7 +2,7 @@
 
 # Insert your variables here
 OPENOCD_CFG_FILE="/usr/share/openocd/scripts/board/stm32f4discovery.cfg"
-PROJECT_PATH="/home/vadimstupakov/GIT/ProjectX/Examples/Boards/STM32F4Discovery/Timer_LED_Blinking/"
+PROJECT_PATH="/home/vadimstupakov/GIT/ProjectX/Examples/Boards/STM32F4Discovery/PWM/"
 STM32_CUBE_MX_INCLUDE_PATH="$HOME/Data/STM32/STM32Cube/Repository/STM32Cube_FW_F4_V1.10.0/"
 
 
